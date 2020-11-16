844e90ed697bde68c39029509a714baaa0cbffcc
Łukasz Szydło <szydler@gmail.com>
2020-11-16 22:35:38 +0100

    Initial commit

---
+++ src/main/java/eu/skillcraft/exercises/ExercisesJavaApplication.java
@@ -0,0 +1,13 @@
+package eu.skillcraft.exercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercisesJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercisesJavaApplication.class, args);
+	}
+
+}
