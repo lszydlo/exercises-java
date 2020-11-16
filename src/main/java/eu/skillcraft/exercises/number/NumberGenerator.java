6ca081216731f6f1c024eb63e46d700672a62ce9
Łukasz Szydło <szydler@gmail.com>
2020-11-16 23:12:44 +0100

    number exercise

---
+++ src/main/java/eu/skillcraft/exercises/number/NumberGenerator.java
@@ -0,0 +1,10 @@
+package eu.skillcraft.exercises.number;
+
+import java.time.YearMonth;
+
+public class NumberGenerator {
+
+	public String generate(int next, String systemType, YearMonth yearMonth, boolean isDemo, boolean isAuditor) {
+		return null;
+	}
+}
