a8f3d0d3e484ece3247c404f65f6426923919636
Łukasz Szydło <szydler@gmail.com>
2020-11-24 09:22:57 +0100

    Revert "handler"
    This reverts commit 87bb1c64

--- src/main/java/eu/skillcraft/exercises/qdoc/preparation/PreparationService.java
+++ src/main/java/eu/skillcraft/exercises/qdoc/preparation/PreparationService.java
@@ -2,14 +2,6 @@ package eu.skillcraft.exercises.qdoc.preparation;
 
 public class PreparationService {
 
-	QDocDraftRepo repo;
 
-	public void handle(DoCreateQDocDraft command) {
-		//
 
-		repo.save(new QDocDraft());
-	}
-
-	static class QDocDraft {
-	}
 }
--- src/main/java/eu/skillcraft/exercises/qdoc/preparation/QDocDraftRepo.java
+++
@@ -1,5 +0,0 @@
-package eu.skillcraft.exercises.qdoc.preparation;
-
-public interface QDocDraftRepo {
-	void save(PreparationService.QDocDraft qDocDraft);
-}
